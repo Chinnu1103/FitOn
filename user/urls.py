ee862f791b0c45950c1a96af805367623ed7c3b3
Chirangivi Bhat <chirangivibhat@gmail.com>
2024-03-12 15:57:47 -0400

    temp

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,5 @@ from .views import authorize_google_fit, callback_google_fit
 app_name="user"
 urlpatterns = [
     path('callback/', callback_google_fit, name='callback_google_fit'),
-    path('authorize/', authorize_google_fit, name='authorize_googlefit'),
+    path('authorize/', authorize_google_fit, name='authorize_google_fit'),
 ]
