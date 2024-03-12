c5d2eb188823b69141a32e695be89f73ba774f33
Chirangivi Bhat <chirangivibhat@gmail.com>
2024-03-12 19:59:11 -0400

    Fix URL

--- metrics/urls.py
+++ metrics/urls.py
@@ -4,5 +4,5 @@ from .views import get_metric_data, list_metrics
 app_name="metrics"
 urlpatterns = [
     path('data/<str:metric>/', get_metric_data, name='get_metric_data'),
-    path('list/', get_metric_data, name='list_metrics'),
+    path('list/', list_metrics, name='list_metrics'),
 ]
